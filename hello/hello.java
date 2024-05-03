37d44098ba002eebbe8bdc93bf43ebe9ec10c4f0
Nguyen Tien Minh <nguyentienminhntm07102004@gmail.com>
2024-05-03 19:33:46 +0700

    Nộp bài gửi lời chào

--- hello/hello.java
+++ hello/hello.java
@@ -1,5 +1,11 @@
-import java.sql.Date;
+import java.util.Scanner;
 
 public class hello{
-  
+  public static void main(String[] args) {
+    System.out.println("Enter your name: ");
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+    System.out.println("Your name is: " + s);
+    sc.close();
+  }
 }
