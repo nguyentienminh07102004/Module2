c0d1142625f191e2f206c8eee7b9558aa7eb3504
Nguyen Tien Minh <nguyentienminhntm07102004@gmail.com>
2024-05-03 20:59:22 +0700

    abc

---
+++ 26_04_2024/UngDungChuyenDoiTienTe/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    index app = new index();
+    app.setVisible(true);
+  }
+}
--- 26_04_2024/UngDungChuyenDoiTienTe/index.java
+++ 26_04_2024/UngDungChuyenDoiTienTe/index.java
@@ -1,7 +1,34 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class index extends JFrame {
+  private int WIDTH = 600;
+  private int HEIGHT = 400;
   public index(){
-    this.setTitle("");
+
+    // Set kích thước và các thông số cho ứng dụng
+    this.setTitle("App");
+    this.setLayout(null);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLocationRelativeTo(null);
+    this.setSize(WIDTH, HEIGHT);
+
+    // Set các component
+    labelCurrency = new JLabel();
+    inputCurrency = new JTextField();
+
+    // Label currency
+    labelCurrency.setText("Tiền tệ đầu vào");
+    labelCurrency.setBounds(20, 20, 100, 30);
+
+    // input currency
+    inputCurrency.setBounds(130, 20, 350, 30);
+
+    this.add(labelCurrency);
+    this.add(inputCurrency);
   }
+
+  private JLabel labelCurrency;
+  private JTextField inputCurrency;
 }
